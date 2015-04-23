536b1b7a767b599296c489864001555433c769ac
gadsc <gadsc@hotmail.com>
2015-04-23 16:57:21 -0300

    Alteração para retornar response

--- cepserver/src/main/java/br/com/cepserver/rest/service/CepService.java
+++ cepserver/src/main/java/br/com/cepserver/rest/service/CepService.java
@@ -6,13 +6,14 @@ import javax.inject.Inject;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import br.com.cepserver.entities.Endereco;
 import br.com.cepserver.service.bean.CepBean;
 
 @Path("/buscarcep")
@@ -25,11 +26,13 @@ public class CepService implements Serializable {
 	@Path("{cep}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	public Endereco buscarEnderecoPorCep(
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response buscarEnderecoPorCep(
 			@Size(min = 9, max = 9, message = "CEP inválido!")
 			@NotNull(message = "CEP inválido!")
 			@Pattern(regexp = "[0-9]{5}-[0-9]{3}", message = "CEP inválido!")
 			@PathParam("cep") String cep) {
-		return cepBean.buscarEnderecoPorCep(cep.replaceAll("-", ""));
+		return Response.ok(
+				cepBean.buscarEnderecoPorCep(cep.replaceAll("-", ""))).build();
 	}
 }
