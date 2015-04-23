b81ecb188dc08a262ed459292eb71fba97205b7f
gadsc <gadsc@hotmail.com>
2015-04-23 17:47:34 -0300

    Chamada do DAO no serviço de endereço

--- cepconsumer/src/main/java/br/com/cepserver/rest/service/EnderecoCrudService.java
+++ cepconsumer/src/main/java/br/com/cepserver/rest/service/EnderecoCrudService.java
@@ -2,6 +2,7 @@ package br.com.cepserver.rest.service;
 
 import java.io.Serializable;
 
+import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.ws.rs.Consumes;
@@ -15,43 +16,46 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import br.com.cepconsumer.dao.DAO;
 import br.com.cepconsumer.entities.Endereco;
 
 @Path("/endereco")
 public class EnderecoCrudService implements Serializable {
 	private static final long serialVersionUID = -2434608870284261408L;
-	
+
+	@Inject
+	private DAO<Endereco> daoEndereco;
+
 	@POST
 	@Path("incluir")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response incluirEndereco(@Valid Endereco endereco) {
-		return null;
+		return Response.ok(daoEndereco.adicionar(endereco)).build();
 	}
-	
+
 	@PUT
 	@Path("alterar")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response alterarEndereco(@Valid Endereco endereco) {
-		return null;
+		return Response.ok(daoEndereco.atualizar(endereco)).build();
 	}
-	
+
 	@GET
 	@Path("buscar/{id}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response buscarEndereco(@PathParam("id")
-		@Min(1) int id) {
-		return null;
+	public Response buscarEndereco(@PathParam("id") @Min(1) int id) {
+		return Response.ok(daoEndereco.pesquisarPorId(id)).build();
 	}
-	
+
 	@DELETE
 	@Path("excluir/{id}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response excluirEndereco(@PathParam("id")
-		@Min(1) int id) {
-		return null;
+	public Response excluirEndereco(@PathParam("id") @Min(1) int id) {
+		daoEndereco.apagarPorId(id);
+		return Response.ok().build();
 	}
 }
